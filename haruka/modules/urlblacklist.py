7346a0ed0e20d7d186b74ee11ed923238b907695
IMVETRI <68427115+IVETRI@users.noreply.github.com>
2021-02-21 02:58:07 -0800

    Update urlblacklist.py

--- haruka/modules/urlblacklist.py
+++ haruka/modules/urlblacklist.py
@@ -158,8 +158,7 @@ URL_DELETE_HANDLER = MessageHandler(
     edited_updates=True)
 
 
-__mod_name__ = "Url 
-Black🛂"
+__mod_name__ = "Url Black🛂"
 
 __help__ = """
 Domain blacklisting is used to stop certain domains from being mentioned in a group, Any time an url on that domain is mentioned, /
