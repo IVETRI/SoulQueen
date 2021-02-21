7bd73d33dd5d10e5dfc4b9fa01de2d2c4ebcdae0
IMVETRI <68427115+IVETRI@users.noreply.github.com>
2021-02-21 02:25:15 -0800

    Update log_channel.py

--- haruka/modules/log_channel.py
+++ haruka/modules/log_channel.py
@@ -159,7 +159,7 @@ Setting the log channel is done by:
 - forwarding the /setlog to the group
 """
 
-    __mod_name__ = "Log Channels"
+    __mod_name__ = "Log Channels🔔"
 
     LOG_HANDLER = CommandHandler("logchannel", logging)
     SET_LOG_HANDLER = CommandHandler("setlog", setlog)
