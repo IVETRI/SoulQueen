708cc949c1035d0a782193fff6e726b8d9ea2ab9
IMVETRI <68427115+IVETRI@users.noreply.github.com>
2021-02-21 02:55:41 -0800

    Update translator.py

--- haruka/modules/translator.py
+++ haruka/modules/translator.py
@@ -30,6 +30,6 @@ def do_translate(bot: Bot, update: Update, args: List[str]):
 
 __help__ = """- /tr (language code) as reply to a long message.
 """
-__mod_name__ = "Translator🐱‍👓"
+__mod_name__ = "Translator🕵🏻‍♂️"
 
 dispatcher.add_handler(DisableAbleCommandHandler("tr", do_translate, pass_args=True))
