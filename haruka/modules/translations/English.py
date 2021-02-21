d03e2fd6004a17c7f08582c4b816cdd777ad4968
IMVETRI <68427115+IVETRI@users.noreply.github.com>
2021-02-21 04:58:08 -0800

    Update English.py

--- haruka/modules/translations/English.py
+++ haruka/modules/translations/English.py
@@ -206,17 +206,15 @@ or feature requests you might have :)
 If you enjoying using me and/or would like to help me survive in the wild, hit /donate to help fund/upgrade my VPS!
 """,
 
-    "send-help": """Hey there! My name is *{}*.
-I'm a modular group management bot with a few fun extras! Have a look at the following for an idea of some of the things I can help you with.
-Main commands available:
- - /start: Cool command to check if the bot is alive or not
- - /help: PM's you this message.
- - /help <module name>: PM's you info about that module.
- - /donate: information about how to donate!
- - /lang: Change bot language
- - /settings:
-   - in PM: will send you your settings for all supported modules.
-   - in a group: will redirect you to pm, with all that chat's settings.
+    "send-help": """Hey there! I'M *{}*.
+*Main commands available*:
+💠 - /start: Cool command to check if the bot is alive or not
+💠 - /help: PM's you this message.
+💠 - /help <module name>: PM's you info about that module.
+💥 - /lang: Change bot language
+💥 - /settings:
+   🔺- in PM: will send you your settings for all supported modules.
+   🔻- in a group: will redirect you to pm, with all that chat's settings.
    {}
    """,
 
