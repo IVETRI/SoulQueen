7b4fdf846d18fef3588d3ab60d9719f31208abe3
IMVETRI <68427115+IVETRI@users.noreply.github.com>
2021-02-21 00:59:50 -0800

    Update antispam.py

--- haruka/modules/antispam.py
+++ haruka/modules/antispam.py
@@ -621,7 +621,7 @@ you and your groups by removing spam flooders as quickly as possible. They can b
 /antispam
 """
 
-__mod_name__ = "Antispam security"
+__mod_name__ = "Antispam🚷"
 
 ANTISPAM_STATUS = CommandHandler("antispam", antispam, pass_args=True, filters=Filters.group)
 
