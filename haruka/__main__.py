d0bc8ccc7382ef370073543c2de1a67eb1bc5914
IMVETRI <68427115+IVETRI@users.noreply.github.com>
2021-07-18 14:59:02 +0530

    Update __main__.py

--- haruka/__main__.py
+++ haruka/__main__.py
@@ -616,7 +616,7 @@ def process_update(self, update):
         return
 
     now = datetime.datetime.utcnow()
-    cnt = CHATS_CNT.get(update.effective_chat.id, 0)
+    cnt = CHATS_CNT.get(update.effective_chat, 0)
 
     t = CHATS_TIME.get(update.effective_chat.id, datetime.datetime(1970, 1, 1))
     if t and now > t + datetime.timedelta(0, 1):
